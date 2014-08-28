cac254fc96adf23fd0504ed455895dacf2b09b57
dushyant <dushmis@gmail.com>
2014-08-29 02:41:04 +0530

    ang

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
-  $http.get('http://2.assignto.appspot.com/posts.json').success(function(data) {
+  $http.get('http://assignto.appspot.com/posts.json').success(function(data) {
     $scope.phones = data.AllPosts;
   });
 
