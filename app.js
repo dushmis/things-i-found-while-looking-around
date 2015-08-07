b739c608dbf3708c4d93deea1db58ffd1271261a
dushyant <dushmis@users.noreply.github.com>
2015-08-07 10:15:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var foundApp = angular.module('thingsIFound', []);
 
 foundApp.controller('FoundCtrl', function ($scope, $http) {
   //yup, that's my domain
-  $http.get('http://hahanoway.xyz/posts.json').success(function(data) {
+  $http.get('http://assignto.appspot.com/posts.json').success(function(data) {
     $scope.posts = data.AllPosts;
   });
 
